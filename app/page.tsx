053a2e5be5a880d1af01c87dcb0f253af41fe604
nsgundam <nsuttibutr0711@gmail.com>
2025-09-15 14:09:47 +0700

    update for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,15 @@ export default function Home() {
 
   return (
     <div>
-      <header className="relative flex pt-3 m-7 gap-5 justify-center">
-
-        <h1 className="text-3xl absolute top-0 left-0 mx-5">Weather Dashboard</h1>
+      <header className="relative md:flex flex-col pt-3 m-7 gap-5 justify-center">
+        <h1 className="text-3xl md:absolute md:top-0 md:left-0 mx-5 py-4">Weather Dashboard</h1>
         <form onSubmit={handleSearch} className="">
           <input
             type="text"
             value={search}
             placeholder="Search City..."
             onChange={(e) => setSearch(e.target.value)}
-            className="absolute top-0 right-0 mx-7 rounded rounded-x py-1 px-5 md:min-w-md bg-[#121A2D]"
+            className="md:absolute md:top-0 md:right-0 mx-7 rounded rounded-x py-1 px-5 md:min-w-md bg-[#121A2D]"
           />
         </form>
       </header>
